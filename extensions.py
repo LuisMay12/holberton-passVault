a2dbc437cb6bdafd70fa370aa2425de643ad4645
samueleangel <samueleangel@gmail.com>
2025-11-13 02:56:37 +0100

    Add CORS support to the application

--- extensions.py
+++ extensions.py
@@ -2,8 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_session import Session
+from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
 # login_manager = LoginManager()
 server_session = Session()
+cors = CORS()
